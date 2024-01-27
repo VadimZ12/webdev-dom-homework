8692f3e765ac0236b5237877f7a9a733f49f1414
Vadim <vadimsvu12@mail.ru>
2024-01-27 17:31:43 +0300

    Сделал обработку лайков

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const addButtonElement = document.getElementById("add-button");
 const addFormElement = document.getElementById("add-form");
-const deleteButtonElement = document.getElementById("delete-button");
 const nameInputElement = document.getElementById("name-input");
 const commentAreaElement = document.getElementById("comment-area");
 const commentsList = document.getElementById("comments-list");
 
-const currentDate = new Date();
-const formattedDateTime = currentDate.toLocaleString('ru-Ru', {
-    year: '2-digit',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit'
-});
+const comments = [
+  {
+    userName: "Глеб Фокин",
+    time: "12.02.22 12:18",
+    commentText: "Это будет первый комментарий на этой странице",
+    likes: "3",
+    isLiked: "",
+  },
+  {
+    userName: "Варвара Н.",
+    time: "13.02.22 19:22",
+    commentText: "Мне нравится как оформлена эта страница! ❤",
+    likes: "75",
+    isLiked: "-active-like",
+  },
+];
 
 nameInputElement.addEventListener('input', handleInputChange);
 commentAreaElement.addEventListener('input', handleInputChange);
@@ -31,6 +38,37 @@ function handleInputChange() {
   }
 }
 
+const likeComments = () => {
+  const likeButtonsElement = document.querySelectorAll(".like-button")
+  
+  for (const likeButtonElement of likeButtonsElement) {
+    likeButtonElement.addEventListener("click", () => {
+      const likeIndex = likeButtonElement.dataset.index;
+
+      if (comments[likeIndex].isLiked) {
+        comments[likeIndex].likes--;
+        comments[likeIndex].isLiked = "";
+      } else {
+        comments[likeIndex].likes++;
+        comments[likeIndex].isLiked = "-active-like";
+      }
+
+      renderComments();
+    });
+  }
+
+};
+
+const deleteComments = () => {
+  const deleteButtonElement = document.getElementById("delete-button");
+
+  deleteButtonElement.addEventListener("click", () => {
+    comments.pop();
+    renderComments();
+  });
+};
+
+
 commentAreaElement.addEventListener("keyup", function(event) {
   if (event.key === 'Enter') {
     addComment();
@@ -39,40 +77,68 @@ commentAreaElement.addEventListener("keyup", function(event) {
 
 addButtonElement.addEventListener("click", addComment);
 
+const renderComments = () => {
+  const commentsHtml = comments
+    .map((comment, index) => {
+      return `<li class="comment">
+      <div class="comment-header">
+        <div>${comment.userName}</div>
+        <div>${comment.time}</div>
+      </div>
+      <div class="comment-body">
+        <div class="comment-text">
+          ${comment.commentText}
+        </div>
+      </div>
+      <div class="comment-footer">
+        <div class="likes">
+          <span class="likes-counter">${comment.likes}</span>
+          <button data-index="${index}" class="like-button ${comment.isLiked}"></button>
+        </div>
+      </div>
+      </li>`
+    })
+    .join("");
+
+  commentsList.innerHTML = commentsHtml;
+
+  likeComments();
+
+};
+
+renderComments();
+likeComments();
+deleteComments();
+
 function addComment() {
   const oldCommentsListHtml = commentsList.innerHTML;
   const name = nameInputElement.value.trim();
   const comment = commentAreaElement.value.trim();
 
   if (name !== '' && comment !== '') {
-    commentsList.innerHTML = oldCommentsListHtml + `<li class="comment">
-    <div class="comment-header">
-      <div>${nameInputElement.value}</div>
-      <div>${formattedDateTime}</div>
-    </div>
-    <div class="comment-body">
-      <div class="comment-text">
-        ${commentAreaElement.value}
-      </div>
-    </div>
-    <div class="comment-footer">
-      <div class="likes">
-        <span class="likes-counter">0</span>
-        <button class="like-button"></button>
-      </div>
-    </div>
-  </li>`
 
-  nameInputElement.value = '';
-  commentAreaElement.value = '';
-  addButtonElement.disabled = true;
-  addButtonElement.classList.add('error');
+    const formattedDateTime = new Date().toLocaleString('ru-Ru', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+
+    comments.push({
+      userName: nameInputElement.value,
+      commentText: commentAreaElement.value,
+      time: `${formattedDateTime}`,
+      likes: 0,
+      isLiked: "",
+    });
+
+    renderComments();
+
+    nameInputElement.value = '';
+    commentAreaElement.value = '';
+    addButtonElement.disabled = true;
+    addButtonElement.classList.add('error');
   }
-
-
 }
-
-deleteButtonElement.addEventListener("click", () => {
-  commentsList.removeChild(commentsList.lastChild);
-});
 
